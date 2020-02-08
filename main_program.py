d36fe2857b347891bca61ea46da257939f43ec81
Coolercool47 <57828244+Coolercool47@users.noreply.github.com>
2020-02-08 12:33:25 +0700

    Замена

--- main_program.py
+++ main_program.py
@@ -1,6 +1,7 @@
 import os
 import pygame
 import random
+import sqlite3
 
 
 def load_image(name):
@@ -12,10 +13,12 @@ def load_image(name):
 class Game:
     def __init__(self):
         self.p = False
-        self.s = True
+        self.s = False
         self.ex = False
         self.sc = int()
         self.en = True
+        self.music = True
+        self.sfx = True
 
     def start(self):
         self.s = True
@@ -61,7 +64,8 @@ class Character(pygame.sprite.Sprite):
         self.touched = False
         self.before_y = 400
 
-    def update(self, g):
+    def update(self, g, state):
+
         t = False
         for x in range(len(objectGroup['titles'])):
             if pygame.sprite.collide_mask(self, objectGroup['titles'][x]) and g.en is True:
@@ -75,9 +79,14 @@ class Character(pygame.sprite.Sprite):
         if t is False:
             self.touched = False
         if self.touched is False or (self.touched is True and self.sc_speed < 0):
-            self.speed += 0.2
+            if state is True:
+                self.speed += 0.3
+            else:
+                self.speed += 0.2
         elif self.touched is True and self.sc_speed > 0:
-            self.speed = -9.5
+            self.speed = -10
+            if game.sfx:
+                jump_sound.play()
             if self.before_y > self.rect.y:
                 self.before_y = self.rect.y
         self.rect = self.rect.move(0, self.speed)
@@ -88,7 +97,6 @@ class Character(pygame.sprite.Sprite):
         if self.rect.x <= -60:
             self.rect.x = 400
         if self.before_y > self.rect.y:
-            g.score(abs(self.before_y - self.rect.y) // 3)
             self.before_y = self.rect.y
 
     def movement(self, x_pos):
@@ -118,6 +126,30 @@ class Character(pygame.sprite.Sprite):
         self.speed = -15
         game.s = False
         game.enable(True)
+        con = sqlite3.connect("scores.db")
+        cur = con.cursor()
+        list_scores = list(cur.execute('''SELECT * FROM Scores'''))
+        con.commit()
+        for k in list_scores:
+            if k[1] > game.sc:
+                continue
+            con = sqlite3.connect("scores.db")
+            cur = con.cursor()
+            cur.execute(f'''UPDATE Scores
+                SET score = {game.sc}
+                where id == {k[0]}''')
+            con.commit()
+            list_scores = list_scores[k[0] - 1:9]
+            break
+        for s in list_scores:
+            if s[1] > game.sc:
+                continue
+            con = sqlite3.connect("scores.db")
+            cur = con.cursor()
+            cur.execute(f'''UPDATE Scores
+                            SET score = {s[1]}
+                            where id == {s[0] + 1}''')
+            con.commit()
 
     def death_from_enemy(self):
         game.enable(False)
@@ -126,6 +158,30 @@ class Character(pygame.sprite.Sprite):
 
     def shooting(self):
         pass
+
+
+class HighScoresButtons(pygame.sprite.Sprite):
+    image = load_image("highscores.png")
+    # color 2e3174
+
+    def __init__(self, group):
+        super().__init__(group)
+        self.sprite = pygame.sprite.Sprite()
+        self.sprite.image = StartButton.image
+        self.rect = self.sprite.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.sprite.image)
+        self.rect.x = 15
+        self.rect.y = 200
+
+    def clicked(self, pos, index):
+        if self.rect.collidepoint(pos):
+            global scores_on
+            scores_on = True
+            e = menuBar.keys()
+            for s in e:
+                if menuBar[s]:
+                    all_other_sprites.remove(menuBar[s][0])
+                    menuBar[s] = []
 
 
 class StartButton(pygame.sprite.Sprite):
@@ -141,6 +197,49 @@ class StartButton(pygame.sprite.Sprite):
         self.rect.x = 10
         self.rect.y = 100
 
+    def clicked(self, pos, index):
+        global just_started
+        if self.rect.collidepoint(pos):
+            game.s = True
+            e = menuBar.keys()
+            for j in e:
+                if menuBar[j]:
+                    all_other_sprites.remove(menuBar[j][0])
+                    menuBar[j] = []
+            just_started = True
+
+
+class BackButton(pygame.sprite.Sprite):
+
+    image = load_image("back.jpg")
+
+    def __init__(self, group):
+        super().__init__(group)
+        self.sprite = pygame.sprite.Sprite()
+        self.sprite.image = StartButton.image
+        self.rect = self.sprite.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.sprite.image)
+        self.rect.x = 10
+        self.rect.y = 10
+
+    def clicked(self, pos, index):
+        global settings_on, scores_on
+        if self.rect.collidepoint(pos):
+            if settings_on:
+                settings_on = False
+                e = settingsBar.keys()
+                for p in e:
+                    if settingsBar[p]:
+                        all_other_sprites.remove(settingsBar[p][0])
+                        settingsBar[p] = []
+            if scores_on:
+                scores_on = False
+                e = settingsBar.keys()
+                for p in e:
+                    if settingsBar[p]:
+                        all_other_sprites.remove(settingsBar[p][0])
+                        settingsBar[p] = []
+
 
 class ExitButton(pygame.sprite.Sprite):
 
@@ -152,8 +251,13 @@ class ExitButton(pygame.sprite.Sprite):
         self.sprite.image = StartButton.image
         self.rect = self.sprite.image.get_rect()
         self.mask = pygame.mask.from_surface(self.sprite.image)
-        self.rect.x = 150
+        self.rect.x = 10
         self.rect.y = 350
+
+    def clicked(self, pos, index):
+        global br
+        if self.rect.collidepoint(pos):
+            br = True
 
 
 class PauseSprite(pygame.sprite.Sprite):
@@ -176,20 +280,121 @@ class PauseSprite(pygame.sprite.Sprite):
         del menuBar['pause'][0]
 
 
-class Title(pygame.sprite.Sprite):
+class SettingsButton(pygame.sprite.Sprite):
 
-    image = load_image("title.jpg")
+    image = load_image('SettingsButton.png')
+
+    def __init__(self, group):
+        super().__init__(group)
+        self.sprite = pygame.sprite.Sprite()
+        self.sprite.image = PauseSprite.image
+        color_key = self.sprite.image.get_at((0, 0))
+        self.sprite.image.set_colorkey(color_key)
+        self.rect = self.sprite.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.sprite.image)
+        self.rect.x = 365
+        self.rect.y = 0
+
+    def remove(self):
+        all_other_sprites.remove(menuBar['pause'][0])
+        del menuBar['pause'][0]
+
+    def clicked(self, pos, index):
+        global settings_on
+        if self.rect.collidepoint(pos):
+            settings_on = True
+            e = menuBar.keys()
+            for j in e:
+                if menuBar[j]:
+                    all_other_sprites.remove(menuBar[j][0])
+                    menuBar[j] = []
+
+
+class MusicButton(pygame.sprite.Sprite):
+
+    def __init__(self, group):
+        if game.music:
+            self.image = load_image('on.png')
+        else:
+            self.image = load_image('off.png')
+        super().__init__(group)
+        self.sprite = pygame.sprite.Sprite()
+        self.sprite.image = self.image
+        self.rect = self.sprite.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.sprite.image)
+        self.rect.x = 10
+        self.rect.y = 350
+
+    def clicked(self, pos, index):
+        if self.rect.collidepoint(pos):
+            global time
+            game.music = not game.music
+            if game.music:
+                self.image = load_image('on.png')
+                time = 0
+            else:
+                self.image = load_image('off.png')
+            self.sprite.image = self.image
+            self.rect = self.sprite.image.get_rect()
+            self.mask = pygame.mask.from_surface(self.sprite.image)
+            self.rect.x = 10
+            self.rect.y = 350
+
+
+class SFXButton(pygame.sprite.Sprite):
+
+    def __init__(self, group):
+        if game.music:
+            self.image = load_image('on.png')
+        else:
+            self.image = load_image('off.png')
+        super().__init__(group)
+        self.sprite = pygame.sprite.Sprite()
+        self.sprite.image = self.image
+        self.rect = self.sprite.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.sprite.image)
+        self.rect.x = 10
+        self.rect.y = 150
+
+    def clicked(self, pos, index):
+        if self.rect.collidepoint(pos):
+            global time
+            game.sfx = not game.sfx
+            if game.sfx:
+                self.image = load_image('on.png')
+                time = 0
+            else:
+                self.image = load_image('off.png')
+            self.sprite.image = self.image
+            self.rect = self.sprite.image.get_rect()
+            self.mask = pygame.mask.from_surface(self.sprite.image)
+            self.rect.x = 10
+            self.rect.y = 150
+
+
+class Tile(pygame.sprite.Sprite):
+
+    image = load_image("сова.png")
 
     def __init__(self, group, x_pos, y_pos):
         super().__init__(group)
         self.sprite = pygame.sprite.Sprite()
-        self.sprite.image = Title.image
+        self.sprite.image = Tile.image
         color_key = self.sprite.image.get_at((0, 0))
         self.sprite.image.set_colorkey(color_key)
         self.rect = self.sprite.image.get_rect()
         self.mask = pygame.mask.from_surface(self.sprite.image)
         self.rect.x = x_pos
         self.rect.y = y_pos
+
+    def movement(self):
+        global running
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(width - 50)
+        if running:
+            self.rect.y = random.randrange(height)
+        else:
+            self.rect.y = random.randrange(-20, -10)
 
     def remove(self):
         all_other_sprites.remove(objectGroup['titles'][0])
@@ -236,7 +441,11 @@ size = width, height = 400, 600
 
 screen = pygame.display.set_mode(size)
 screen.fill((0, 0, 0))
-
+br = False
+settings_on = False
+time = 1500.0
+main_theme = pygame.mixer.Sound(os.path.join('sounds', 'main_theme.wav'))
+jump_sound = pygame.mixer.Sound(os.path.join('sounds', 'jump.wav'))
 game = Game()
 objectGroup = dict()
 menuBar = dict()
@@ -245,6 +454,12 @@ objectGroup['enemy'] = []
 menuBar['pause'] = []
 menuBar['exit'] = []
 menuBar['start'] = []
+menuBar['high_scores'] = []
+menuBar['settings'] = []
+settingsBar = dict()
+settingsBar['music'] = []
+settingsBar['sfx'] = []
+settingsBar['back'] = []
 running = True
 MY_EVENT_TYPE = 10
 all_other_sprites = pygame.sprite.Group()
@@ -252,41 +467,166 @@ character_sprite = pygame.sprite.Group()
 character = Character(character_sprite)
 pause = PauseSprite(all_other_sprites)
 all_other_sprites.remove(pause)
-title = Title(all_other_sprites, 200, 350)
+title = Tile(all_other_sprites, 200, 350)
 objectGroup['titles'].append(title)
-title = Title(all_other_sprites, 200, 125)
+title = Tile(all_other_sprites, 200, 125)
 objectGroup['titles'].append(title)
 enemy = Enemy(all_other_sprites, 100, 100)
 objectGroup['enemy'].append(enemy)
+SettingButton = SettingsButton(all_other_sprites)
+menuBar['settings'].append(SettingButton)
+ScoreBoard = HighScoresButtons(all_other_sprites)
+menuBar['high_scores'].append(ScoreBoard)
 pygame.time.set_timer(MY_EVENT_TYPE, 10)
 mouse_motion_pos_x = int()
 font = pygame.font.Font(None, 20)
 score = font.render("Score: ", 1, (80, 80, 80))
+just_started = False
+scores_on = False
 score_x = 300
 score_y = 10
 sc_x = 350
 sc_y = 10
+a = 0
+font2 = pygame.font.Font(None, 50)
+clock = pygame.time.Clock()
 pause_font = pygame.font.Font(None, 50)
+b = False
 while running:
+    if game.music:
+        if time == 1550:
+            time = 0
+            main_theme.play()
     for event in pygame.event.get():
         screen.fill((255, 255, 255))
         sc = font.render(f"{game.get_score()}", 1, (80, 80, 80))
         character_sprite.draw(screen)
         all_other_sprites.draw(screen)
-        if game.s is True:
-            screen.blit(sc, (sc_x, sc_y))
-            screen.blit(score, (score_x, score_y))
         if event.type == MY_EVENT_TYPE and game.p is False:
-            character.update(game)
+            character.update(game, b)
         if event.type == pygame.QUIT:
             running = False
-        if game.s is True:
+        if game.s and not settings_on and not scores_on:
+            for i in objectGroup['enemy']:
+                if i.rect.y > 1000:
+                    objectGroup['enemy'] = []
             if event.type == pygame.MOUSEMOTION:
                 mouse_motion_pos_x, mouse_motion_pos_y = event.pos
+            screen.blit(sc, (sc_x, sc_y))
+            screen.blit(score, (score_x, score_y))
+            for i in range(len(objectGroup['titles'])):
+                if objectGroup['titles'][i].rect.y > 600:
+                    del objectGroup['titles'][i]
+                    break
+            if len(objectGroup['titles']) < 7 and just_started:
+                list_cords = []
+                yy = []
+                for i in objectGroup['titles']:
+                    list_cords.append((i.rect.x, i.rect.y))
+                    yy.append(i.rect.y)
+                for i in range(7 - len(objectGroup['titles'])):
+                    x = random.randrange(0, 350)
+                    y = random.randrange(0, max(yy) - 70)
+                    for j in list_cords:
+                        while (x + 74 < j[0] or x > j[0] + 74) and (y + 20 < j[1] or x > j[1] + 20):
+                            x = random.randrange(0, 350)
+                            y = random.randrange(0, max(yy) - 40)
+                    tile = Tile(all_other_sprites, x, y)
+                    objectGroup['titles'].append(tile)
+                just_started = False
+            elif len(objectGroup['titles']) < 7 and not just_started:
+                list_cords = []
+                yy = []
+                g = random.randrange(10)
+                for i in objectGroup['titles']:
+                    list_cords.append((i.rect.x, i.rect.y))
+                    yy.append(i.rect.y)
+                for i in range(7 - len(objectGroup['titles'])):
+                    x = random.randrange(0, 350)
+                    y = random.randrange(-10, 10)
+                    for j in list_cords:
+                        while (x + 74 < j[0] or x > j[0] + 74) and (y + 20 < j[1] or x > j[1] + 20):
+                            x = random.randrange(0, 350)
+                            y = random.randrange(-10, 10)
+                    tile = Tile(all_other_sprites, x, y)
+                    objectGroup['titles'].append(tile)
+                if not objectGroup['enemy']:
+                    x = random.randrange(0, 350)
+                    y = random.randrange(-200, -100)
+                    enemy = Enemy(all_other_sprites, x, y)
+                    objectGroup['enemy'].append(enemy)
+            if character.rect.y < 200 and not game.p:
+                b = True
+                a = 1
+
+            if character.rect.y > 250 and not game.p:
+                b = False
+                if character.rect.y < 400:
+                    a = 2
+
+            if character.rect.y < 10 and not game.p:
+                for i in objectGroup['titles']:
+                    i.rect = i.rect.move(0, 9)
+                for i in objectGroup['enemy']:
+                    i.rect = i.rect.move(0, 9)
+
+            elif b is True and game.p is False:
+                for i in objectGroup['titles']:
+                    i.rect = i.rect.move(0, 4)
+                for i in objectGroup['enemy']:
+                    i.rect = i.rect.move(0, 4)
+
+        elif settings_on:
+            text = font2.render("Music", 10, (0, 0, 0))
+            text_x = 25
+            text_y = 300
+            screen.blit(text, (text_x, text_y))
+            text1 = font2.render("SFX", 10, (0, 0, 0))
+            text1_x = 35
+            text1_y = 100
+            screen.blit(text1, (text1_x, text1_y))
+            if len(settingsBar['music']) == 0:
+                msc = MusicButton(all_other_sprites)
+                settingsBar['music'].append(msc)
+            if len(settingsBar['sfx']) == 0:
+                msc = SFXButton(all_other_sprites)
+                settingsBar['sfx'].append(msc)
+            if len(settingsBar['back']) == 0:
+                back = BackButton(all_other_sprites)
+                settingsBar['back'].append(back)
+        elif scores_on:
+            if len(settingsBar['back']) == 0:
+                back = BackButton(all_other_sprites)
+                settingsBar['back'].append(back)
+            text = font2.render("HIGH SCORES", 1, (0, 0, 100))
+            text_x = 10
+            text_y = 50
+            screen.blit(text, (text_x, text_y))
+            con = sqlite3.connect("scores.db")
+            cur = con.cursor()
+            list_scores = list(cur.execute('''SELECT * FROM Scores'''))
+            con.commit()
+            a = 40
+            for i in range(len(list_scores)):
+                text = font2.render(f'{i + 1}: {list_scores[i][1]}', 1, (0, 0, 100))
+                text_x = 10
+                text_y = a * (i + 1) + 50
+                screen.blit(text, (text_x, text_y))
+
         else:
+            game.sc = 0
             if len(menuBar['start']) == 0:
                 start_button = StartButton(all_other_sprites)
                 menuBar['start'].append(start_button)
+            elif len(menuBar['exit']) == 0:
+                start_button = ExitButton(all_other_sprites)
+                menuBar['exit'].append(start_button)
+            elif len(menuBar['settings']) == 0:
+                settings_button = SettingsButton(all_other_sprites)
+                menuBar['settings'].append(settings_button)
+            elif len(menuBar['high_scores']) == 0:
+                ScoreBoard = HighScoresButtons(all_other_sprites)
+                menuBar['high_scores'].append(ScoreBoard)
             else:
                 menuBar['start'][0].update()
                 mouse_motion_pos_x = character.rect.x
@@ -294,12 +634,21 @@ while running:
                     objectGroup['titles'][0].remove()
                 if len(objectGroup['enemy']) != 0:
                     objectGroup['enemy'][0].destroy()
-                title = Title(all_other_sprites, 250, 450)
+                title = Tile(all_other_sprites, 225, 400)
                 objectGroup['titles'].append(title)
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 if game.s is True:
                     game.pause()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                a = menuBar.keys()
+                for i in a:
+                    if menuBar[i]:
+                        menuBar[i][0].clicked(event.pos, i)
+                for i in settingsBar.keys():
+                    if settingsBar[i]:
+                        settingsBar[i][0].clicked(event.pos, i)
         if game.p is False:
             if len(menuBar['pause']) != 0:
                 menuBar['pause'][0].remove()
@@ -310,4 +659,17 @@ while running:
             if len(menuBar['pause']) == 0:
                 pause = PauseSprite(all_other_sprites)
                 menuBar['pause'].append(pause)
+    if game.music:
+        time += 2.5
+    else:
+        time = 1550
+    if a == 2:
+        game.sc += 2
+        a = 0
+    elif a == 1:
+        game.sc += 1
+        a = 0
+    clock.tick(100)
     pygame.display.flip()
+    if br:
+        break
